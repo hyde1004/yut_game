c55442f6b5c767e49056c6ff2765f4796e0818d3
Junggu Lee <hyde1117@naver.com>
2014-03-21 22:01:07 +0900

    init version

---
+++ tc_stick.rb
@@ -0,0 +1,16 @@
+require 'test/unit'
+
+class TestStick < Test::Unit::TestCase
+	def test_true
+		assert(true)
+	end
+
+	def test_add
+		assert_equal(2, 1+1)
+	end
+
+	def test_Stick_object_exist
+			
+	end	
+	
+end
